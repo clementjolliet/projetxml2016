3b036bbe7b6bd684204c09ec8319943a5993c2ed
Guillaume Lerda <lerda.guillaume@gmail.com>
2016-12-11 16:09:28 +0100

    Navigation yolo

---
+++ app/navigation_app.component.ts
@@ -0,0 +1,11 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'navig_app',
+   templateUrl: './app/navigation/navigation.html'
+   styleUrls: ['./app/navigation/navigation.css']
+})
+
+export class AppComponent {
+	
+}
