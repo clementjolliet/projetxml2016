58ad39aa49ecdb209002b87248d1726db83763d8
Guillaume Lerda <lerda.guillaume@gmail.com>
2016-12-12 18:19:53 +0100

    Début affichage images

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+var rawData = [];
+
 try {
 	var http = require("http");
+	var https = require("https");
 	var querystring = require('querystring');
 	var url = require('url');
 	var fs = require("fs");
 	var express = require('express');
-	var sparql = require('sparql-client');
+	var wdk = require('wikidata-sdk')
+	var util = require('util');
 } catch (ex) {
     handleErr(ex);
 	console.log("Echec chargement framework node");
@@ -45,10 +49,56 @@ var app = express.createServer(function(req, res) {
 
     // Appel HTTP pour effectuer la rechercher sur la BD
     http.get(urlHTTP, (result) => {
-		  const statusCode = result.statusCode;
-		  const contentType = result.headers['content-type'];
+		  result.setEncoding('utf8');
+		  rawData = '';
+		  result.on('data', (chunk) => rawData += chunk);
+		  result.on('end', () => {
+			    try {
+			    	rawData = JSON.parse(rawData);
 
-		  let error;
+			    	for (var i=0; i<rawData.length; i++)
+			    	{
+
+			    		monumentData = rawData[i];
+			    		var query = 'SELECT DISTINCT (GROUP_CONCAT(DISTINCT ?merimee; SEPARATOR = ", ") AS ?merimee) ?coords ?image WHERE { { SELECT DISTINCT ?item ?merimee WHERE { ?item (wdt:P1435/wdt:P279*) wd:Q916475. ?item p:P1435 ?heritage_statement. ?item wdt:P380 ?merimee. FILTER(REGEX(?merimee, "'+ monumentData['REF'] +'")) FILTER(NOT EXISTS { ?heritage_statement pq:P582 ?end. }) } } OPTIONAL { ?item wdt:P625 ?coords. } OPTIONAL { ?item wdt:P18 ?image. } SERVICE wikibase:label { bd:serviceParam wikibase:language "fr". } } GROUP BY ?item ?coords ?image';
+			    		var urlQuery = wdk.sparqlQuery(query);
+			    		https.get(urlQuery, (result2) => {
+			    			result2.setEncoding('utf8');
+		  					var rawData2 = '';
+		  					result2.on('data', (chunk2) => rawData2 += chunk2);
+		  					result2.on('end', () => {
+		  						rawData2 = JSON.parse(rawData2);
+		  						imageData = rawData2['results']['bindings'][0]['image'];
+		  						var imageUrl = '';
+		  						if (imageData)
+		  						{
+		  							imageUrl = imageData['value'];
+		  							setRawDataParameter(i, 'IMG', imageUrl);
+		  						}
+							});
+			    		}).on('error', (e) => {
+		  					console.log(`Got error: ${e.message}`);
+		  				});
+			    	}
+
+			    	rawData = JSON.stringify(rawData);
+			      	res.write(rawData);
+					res.end();
+			    } catch (e) {
+			      console.log(e.message);
+			    }
+		  	});
+		}).on('error', (e) => {
+		  console.log(`Got error: ${e.message}`);
+	});
+});
+
+function afficheMessageErreurHTTP(resultat)
+{
+	const statusCode = result.statusCode;
+	const contentType = result.headers['content-type'];
+
+	let error;
 		  if (statusCode !== 200) {
 		    error = new Error(`Request Failed.\n` +
 		                      `Status Code: ${statusCode}`);
@@ -62,23 +112,11 @@ var app = express.createServer(function(req, res) {
 		    result.resume();
 		    return;
 		  }
+}
 
-		  result.setEncoding('utf8');
-		  let rawData = '';
-		  result.on('data', (chunk) => rawData += chunk);
-		  result.on('end', () => {
-			    try {
-
-			    var query = "SELECT DISTINCT ?item (group_concat(distinct ?merimee ; separator = ', ') as ?merimee) ?coords ?image WHERE { { SELECT DISTINCT ?item ?merimee  WHERE { FILTER regex(?merimee, 'PA00078014') . ?item wdt:P1435/wdt:P279* wd:Q916475 . ?item p:P1435 ?heritage_statement . FILTER NOT EXISTS { ?heritage_statement pq:P582 ?end . } ?item wdt:P380 ?merimee. } ORDER BY ?merimee } ?item wdt:P131/wdt:P131* wd:Q142 . OPTIONAL { ?item wdt:P625 ?coords . } OPTIONAL { ?item wdt:P18 ?image . } SERVICE wikibase:label { bd:serviceParam wikibase:language 'fr' . } } GROUP BY ?item  ?coords ?image";
-			      	res.write(rawData);
-					res.end();
-			    } catch (e) {
-			      console.log(e.message);
-			    }
-		  	});
-		}).on('error', (e) => {
-		  console.log(`Got error: ${e.message}`);
-	});
-});
+function setRawDataParameter(position, key, valeur)
+{
+	rawData[position][key] = valeur;
+}
 
 app.listen(1337);
